921ff39ecdec0ed4a4ad5dc1b0b6784cff714165
ABDULRAHMAN ALASSAF <abodoassaf112@gmail.com>
2024-12-29 15:34:26 +0800

    adding the name column to students and teachers tables

--- database/migrations/2024_12_27_081640_create_students_table.php
+++ database/migrations/2024_12_27_081640_create_students_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,6 +14,7 @@ return new class extends Migration
             $table->id("studentID");
             $table->unsignedBigInteger("userID");
             $table->unsignedBigInteger("classID")->nullable(true);
+            $table->string("name");
             $table->timestamps();
             $table->foreign('userID')->references('userID')->on('users')->onDelete('cascade');
             $table->foreign('classID')->references('classID')->on('classes')->onDelete('set null');
