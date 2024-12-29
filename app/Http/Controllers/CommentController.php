574eb31744962b5195157d82826500369a6a5cc3
ABDULRAHMAN ALASSAF <abodoassaf112@gmail.com>
2024-12-29 15:35:25 +0800

    Comment controller finished

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    //
+    public function addComment(Request $request)
+    {
+        try {
+            $studentID = $request->input("studentID");
+            $teacherID = $request->input("teacherID");
+            $comment = $request->input("comment");
+            $commentType = $request->input("commentType");
+            $date = $request->input("date");
+
+            $insertedComment = Comment::create([
+                "studentID" => $studentID,
+                "teacherID" => $teacherID,
+                "commentType" => $commentType,
+                "comment" => $comment,
+                "date" => $date
+            ]);
+
+            if ($insertedComment) {
+                return response()->json([
+                    "status" => "success",
+                    "message" => "Comment added successfully."
+                ]);
+            } else {
+                return response()->json([
+                    "status" => "error",
+                    "message" => "Failed to add the comment."
+                ]);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => "error",
+                "message" => "An error occurred while adding the comment.",
+                "error" => $e->getMessage()
+            ], 500);
+        }
+    }
+
+}
